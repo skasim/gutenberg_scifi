cc57d5474aeb1896b5e4f1080f55fb4e04079f6f
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-09-15 08:37:16 -0400

    nathan's edits and comments

--- engine/engine_json.py
+++ engine/engine_json.py
@@ -10,33 +10,7 @@ import json
 #/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/json/__init__.pyc
 import os
 
-python_dict = {
-    "id": 1,
-    "name": "A green door",
-    "price": 12.50,
-    "tags": ["home", "green"]
-}
-filename = "test.json"
-
-
 #### Create JSON Engine
-
-def create_json(filename):
-    'creates json file'
-    if os.path.isfile(filename):
-        print "file exists"
-    else:
-        with open (filename, "w") as f:
-            print "creating file"
-            pass
-            
-def write_json(filename, python_dict):
-    "writes json object"
-    with open (filename, "w") as filename:
-        data = json.dump(python_dict, filename)
-        
-    return "written to file"
-     
 
 def load_json(filename):
     "loads json object"
@@ -46,17 +20,43 @@ def load_json(filename):
     
     return data
 
+def create_json(filename):
+    'creates json file'
+    with open (filename, "w") as json_file:
+        print "creating file"
+        data = json.load(json_file)
+        return data
+            
+def write_json(filename, python_dict):
+    "writes json object"
+    with open (filename, "w") as filename:
+        data = json.dump(python_dict, filename)
+        
+    return "written to file"
+     
 
-def load_or_create_json():
+
+
+
+def load_or_create_json(filename):
     ' Checks to see if json object exists and then loads it, else creates json and loads it '
     #to check if something exists use os
-    pass
+
+    if os.path.isfile(filename):
+        data = load_json(filename)
+        
+    else:
+        data = create_json(filename)
+
+    return data # returns a python dict
+
+
 
 
 #### Read / Write of keys into json object
 
 
-def read_json(key):
+def read_json(key, filename=None): # Added key value argument for file
     '''
     checks loaded json object for key
 
@@ -65,12 +65,13 @@ def read_json(key):
     else returns, None / False
 
     '''
-         
-    data = load_json(filename)
+       
+    data = load_or_create_json(filename)
     
-    
+    # dictionaries have a get method... try it... what does it do?
+
     if key in data:
-        print key, data[key]
+        return {key: data[key]} # returns it as a dictionary
     else: 
         
         return None
@@ -83,21 +84,26 @@ def write_dict_json(python_dict):
     data = load_json(filename)
     python_dict[new_k] = [new_v]
     
-    print python_dict
+    # What about the write_json function to dump the python dict to a file? 
+
+    print python_dict # print is for human, return is for computers (and humans on computers!!!)
+
 
 
 ##### Data Store (json) engine
 
-def json_read_engine(python_dict):
+def json_read_engine(filename=None, key=None):
     """
     Checks if json datastore is created, if not creates it
 
     Checks for key in datastore and returns value, else returns none
 
     """
-    create_json(filename)
+    create_json(filename) # load_or_create? 
     
-    write_dict_json(python_dict)
+    # read_json? Or a custom function? Maybe the dictionary.get method?
+
+
 
 def json_write_engine(python_dict):
     """
@@ -106,6 +112,15 @@ def json_write_engine(python_dict):
     Checks for for key in datastore, if yes updates key-values, otherwise adds new key-value
 
     """
+    # This is a good attempt, it's evident that you're building a great understanding of functions and variables
+    # I recommend you practice passing in variables into the function example: function(var1, var2, var3=None)
+    # This will make your code much easier to read and recycle later on for other projects. 
+    # You won't need to worry about global variables such as new_k or new_v not being there and breaking things.
+
+    # For both the read and write methods of the json_engine, I recommend that you think about your helper functions and
+    # designing them to reduce your workload (and amount of code to write). We can review this. =)
+    
+
     create_json(filename)
     datastore = read_json(key)
     
@@ -114,6 +129,15 @@ def json_write_engine(python_dict):
     else:
         python_dict[key] = [new_v]
 
+
+
+python_dict = {
+    "id": 1,
+    "name": "A green door",
+    "price": 12.50,
+    "tags": ["home", "green"]
+}
+filename = "test.json"
 
 create_json(filename)
 write_json(filename, python_dict)
