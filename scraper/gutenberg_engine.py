4353d595555149b8379a1745d9c8eb22cb55e613
samxj <skasim@gmail.com>
2015-10-25 13:39:58 -0400

    updated unit_tests.py

--- scraper/gutenberg_engine.py
+++ scraper/gutenberg_engine.py
@@ -4,18 +4,25 @@
 
 from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
+from scraper.crawler import open_csv
 
 def get_book_text(csvfile):
     'gets text for book using project gutenberg catalog'
     book_list = open_csv(csvfile)
-    
-    for i in range(len(book_list)):
-    
-        a = int(book_list[i][2]) # a = book number
-        print a
-        text = strip_headers(load_etext(a)).strip()
+ 
         
-        return(text)  # prints 'MOBY DICK; OR THE WHALE\n\nBy Herman Melville ...'
+    for i, value in enumerate(book_list):
+        #print i, value
+        
+        a = int(book_list[i][2]) # a = book number
+        print i, a
+        author = book_list[i][0]
+        title = book_list[i][1]
+        try:
+            text = strip_headers(load_etext(a)).strip()
+        except ValueError:
+            pass
+        #return text # prints 'MOBY DICK; OR THE WHALE\n\nBy Herman Melville ...'
 
 """
 from gutenberg.acquire import load_etext
